d10b901dc36c1edefcf8d6f62af50983d0e59389
edoardo <edovi@icloud.com>
2023-12-31 21:14:42 +0100

    Adding split

--- Document.py
+++ Document.py
@@ -1,4 +1,4 @@
-from collections import Counter, defaultdict
+from collections import Counter, defaultdict, namedtuple
 import numpy as np
 import nltk
 from nltk.stem.snowball import EnglishStemmer
@@ -6,6 +6,7 @@ from nltk.stem import WordNetLemmatizer
 import re
 from math import sqrt
 
+SplitReturn = namedtuple("SplitReturn", ["left", "right", "size_left", "size_right", "distance"])
 
 # I.e. Document in a Bag Of Word representation
 class Document:
@@ -14,6 +15,7 @@ class Document:
             raise()
         self._stemmed = stemmed
         self._path = path
+        self._tokens = []
         self._counts = Counter()
         self.stemmer = EnglishStemmer()
         self.lemmatizer = WordNetLemmatizer()
@@ -23,24 +25,39 @@ class Document:
                 self._lines = f.readlines()
         else:
             self._lines = lines
+       
+        lines = self._lines
+        for l in lines:
+            tok = self.tokenize(l)
+            tok_ = []
+            # Join two consecutive tokens
+            i = 0
+            while i < len(tok):
+                if i + 1 >= len(tok):
+                    tok_.append(tok[i])
+                else:
+                    if tok[i] == tok[i+1]:
+                        j = 1
+                        new_token = tok[i]
+                        #print("! found two identical consecutive tokens :", tok[i])
+                        while i+j < len(tok) and tok[i+j] == tok[i]:
+                            new_token += f"_{tok[i+j]}"
+                            j += 1
+                        tok_.append(new_token)
+                        i += j
+                    else:
+                        tok_.append(tok[i])
+                i += 1
 
-    @classmethod
-    def from_lines(cls, lines, stemmed=False):
-        instance = cls("NOPE")
-        instance._lines = lines
+            self._tokens = self._tokens + tok_
+
+        # print(tokens)
     
     def tokenize(self, text):
         return nltk.word_tokenize(text)
 
     def make_bow(self):
-        tokens = []
-        lines = self.lines()
-        for l in lines:
-               tok = self.tokenize(l)
-               tokens = tokens + tok
-
-        # print(tokens)
-        self._counts = Counter(tokens)
+        self._counts = Counter(self._tokens)
         self.remove_stopwords()
         self.lemmatize()
         return self
@@ -54,7 +71,10 @@ class Document:
             return self._counts[key]
         except:
             return 0
-
+        
+    def N(self):
+        return len(self._tokens)
+    
     def contains(self, word):
         return word in self._counts
 
@@ -93,8 +113,13 @@ class Document:
     def lines(self):
         return self._lines
     
+    def distance(self, other):
+       # Relegate to cosine distance
+       return self.cosine_distance(other)
+    
     def cosine_distance(self, other):
-        
+        if self.N() == 0 or other.N() == 0:
+            return 1
         # First vector count. Build a defaultdict whose first argument is 'int' from the `counts' dict.
         # basically, will return the standard count if the key is present, 0 otherwise. 
         u = defaultdict(int, self._counts)
@@ -112,22 +137,33 @@ class Document:
         for k in keys:
             # Remember: it's 0 if it's not present
             u_dot_v += u[k] * v[k]
-        # Cosine, rounded
+        # Cosine distance (1-cosine similarity), rounded
         return round(1-(u_dot_v / (norm_u * norm_v)), 4)
 
-    def find_split(self):
-        n = int(len(self._lines) / 4)
-        d1 = Document(lines=self._lines[0:n]).make_bow()
-        d2 = Document(lines=self._lines[n:]).make_bow()
-        return d1, d2, d1.cosine_distance(d2)
+    def split(self, n):
+        if self.N() == 1:
+            d1 = Document(lines=self._tokens).make_bow()
+            d2 = Document(lines=[]).make_bow()
+        else:
+            d1 = Document(lines=self._tokens[:n]).make_bow()
+            d2 = Document(lines=self._tokens[n:]).make_bow()
+        return SplitReturn(left=d1, right=d2, size_left=d1.size(), size_right=d2.size(), distance=d1.distance(d2))
+    
+    def split_half(self):
+        n = int(self.N()/2)
+        return self.split(n)
+        
 
     def text(self, escape = False):
-        t = " ".join(self.lines())
+        t = " ".join(self._tokens)
         if escape:
             return re.escape(t)
         else:
             return t
     
+    def size(self): 
+        return int(sum([len(s.encode('utf-8')) for s in self._tokens]) / 1)#(1024 * 1024)
+
     def counts(self):
         return self._counts
     
